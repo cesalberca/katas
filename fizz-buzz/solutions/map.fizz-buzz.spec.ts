9920dbf30d23eafd3e58e202af0ed0ffb2c7e072
César Alberca <cesalberca@gmail.com>
2020-12-04 16:16:42 +0100

    Rename kata

--- fizz-buzz/solutions/map.fizz-buzz.spec.ts
+++ fizz-buzz/solutions/map.fizz-buzz.spec.ts
@@ -1,10 +1,10 @@
-import { fizzBuzz } from './fizz-buzz'
+import { mapFizzBuzz } from './map.fizz-buzz'
 
 describe('fizzBuzz', () => {
   it('should replace the 3 by fizz', () => {
     const given = 3
 
-    const actual = fizzBuzz(given)
+    const actual = mapFizzBuzz(given)
 
     expect(actual).toEqual([1, 2, 'fizz'])
   })
@@ -12,7 +12,7 @@ describe('fizzBuzz', () => {
   it('should replace the 5 by buzz', () => {
     const given = 5
 
-    const actual = fizzBuzz(given)
+    const actual = mapFizzBuzz(given)
 
     expect(actual).toEqual([1, 2, 'fizz', 4, 'buzz'])
   })
@@ -20,14 +20,14 @@ describe('fizzBuzz', () => {
   it('should replace all numbers whose modulus is 0 when divided by 3', () => {
     const given = 6
 
-    const actual = fizzBuzz(given)
+    const actual = mapFizzBuzz(given)
 
     expect(actual).toEqual([1, 2, 'fizz', 4, 'buzz', 'fizz'])
   })
 
   it('should replace all numbers whose modulus is 0 when divided by 3 and 5 to fizzbuzz', () => {
     const given = 15
-    const result = fizzBuzz(given)
+    const result = mapFizzBuzz(given)
     expect(result).toEqual([
       1,
       2,
--- fizz-buzz/solutions/map.fizz-buzz.ts
+++ fizz-buzz/solutions/map.fizz-buzz.ts
@@ -1,4 +1,4 @@
-export function fizzBuzz(number: number) {
+export function mapFizzBuzz(number: number) {
   return Array.from({ length: number }, (_, k) => k + 1).map(x =>
     x % 3 === 0 ? (x % 5 === 0 ? 'fizzbuzz' : 'fizz') : x % 5 === 0 ? 'buzz' : x
   )
