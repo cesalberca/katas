56fbd29f8ea36431f06c4651c717846c421de025
César Alberca <cesalberca@gmail.com>
2020-12-21 11:01:40 +0100

    WIP

--- christmas-tree/solutions/christmas-tree.ts
+++ christmas-tree/solutions/christmas-tree.ts
@@ -1,24 +1,29 @@
 export class ChristmasTree {
-  private starSymbol = '*'
-
   render(rows: number): string {
-    let tree = ''
     let maxStarsPerRow = rows + rows - 1
 
+    const treeArr: string[] = []
+
     for (let i = 0; i < rows; i++) {
+      const currentStarsInRow = i + i + 1
       let row = ''
-      for (let j = 0; j < maxStarsPerRow; j++) {
-        const currentStarsInRow = i + i + 1
-        const shouldPaintBlankSpace = 1
-        if (true) {
-          row += this.starSymbol
-        } else {
-          row += ' '
-        }
+      for (let j = 0; j < currentStarsInRow; j++) {
+        row += '*'
       }
-      tree += row + '\n'
+      treeArr.push(row)
     }
 
+    const finalTree = treeArr
+      .map((x, i) => {
+        const paddedSpaces = Math.floor(maxStarsPerRow / 2) + i * -1
+        return x.padStart(paddedSpaces).padEnd(paddedSpaces)
+      })
+      .join('\n')
+
+    return this.addTrunk(maxStarsPerRow, finalTree + '\n')
+  }
+
+  private addTrunk(maxStarsPerRow: number, tree: string) {
     let lastRow = ''
     const half = Math.floor(maxStarsPerRow / 2)
     for (let i = 0; i < maxStarsPerRow; i++) {
