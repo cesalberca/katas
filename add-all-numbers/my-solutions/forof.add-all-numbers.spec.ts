e69db6dd55ba1f12b575f60fa56ffe9899ae6ffd
GermanDG <gdelgado@civitfun.com>
2022-08-08 16:20:39 +0100

    Implementing the kata with TDD and 'for of' statement

---
+++ add-all-numbers/my-solutions/forof.add-all-numbers.spec.ts
@@ -0,0 +1,18 @@
+import { addAllNumbers } from './forof.add-all-numbers'
+describe('addAllnumbers', () => {
+  it('given an array of a number should return the number', () => {
+    const given = [1]
+
+    const result = addAllNumbers(given)
+
+    expect(result).toBe(1)
+  })
+
+  it('given an array of numbers should return the total of all numbers', () => {
+    const given = [1, 2, 3, 4, 5]
+
+    const result = addAllNumbers(given)
+
+    expect(result).toBe(15)
+  })
+})
---
+++ add-all-numbers/my-solutions/forof.add-all-numbers.ts
@@ -0,0 +1,7 @@
+export function addAllNumbers(numbers: number[]) {
+  let total = 0
+  for (const number of numbers) {
+    total += number
+  }
+  return total
+}
