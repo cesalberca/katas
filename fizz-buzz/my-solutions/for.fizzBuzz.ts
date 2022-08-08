9da2355c1cea0cd34c2c89984322d270b5c3fe18
GermanDG <gdelgado@civitfun.com>
2022-08-08 18:46:53 +0100

    refactor :hammer:

--- fizz-buzz/my-solutions/for.fizzBuzz.ts
+++ fizz-buzz/my-solutions/for.fizzBuzz.ts
@@ -1,13 +1,14 @@
-export function fizzBuzz(n: number) {
-  let listOfNumbers = []
-  for (let i = 1; i <= n; i++) {
-    i % 3 === 0 && i % 5 === 0
-      ? listOfNumbers.push('fizzbuzz')
-      : i % 3 === 0
-      ? listOfNumbers.push('fizz')
-      : i % 5 === 0
-      ? listOfNumbers.push('buzz')
-      : listOfNumbers.push(i)
+export function fizzBuzz(number: number) {
+  const listOfNumbers = []
+  for (let i = 1; i <= number; i++) {
+    const isDivisibleByThree = i % 3 === 0
+    const isDivisibleByFive = i % 5 === 0
+    let value: number | string = ''
+    if (isDivisibleByThree) value = 'fizz'
+    if (isDivisibleByFive) value = 'buzz'
+    if (isDivisibleByThree && isDivisibleByFive) value = 'fizzbuzz'
+    if (value === '') value = i
+    listOfNumbers.push(value)
   }
   return listOfNumbers
 }
