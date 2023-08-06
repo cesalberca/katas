2ef39e5e620f93debb08d15b1654bc6644d732f8
clemgbld <clementgombauld@hotmail.fr>
2023-08-06 15:25:45 +0200

    implemented christmas tree with recursion

---
+++ src/sum-of-pair/my-solutions/sumOfPair.ts
@@ -0,0 +1,39 @@
+// sum_pairs([11, 3, 7, 5],         10)
+// #              ^--^      3 + 7 = 10
+// == [3, 7]
+
+// sum_pairs([4, 3, 2, 3, 4],         6)
+// #          ^-----^         4 + 2 = 6, indices: 0, 2 *
+// #             ^-----^      3 + 3 = 6, indices: 1, 3
+// #                ^-----^   2 + 4 = 6, indices: 2, 4
+// #  * entire pair is earlier, and therefore is the correct answer
+// == [4, 2]
+
+// sum_pairs([0, 0, -2, 3], 2)
+// #  there are no pairs of values that can be added to produce 2.
+// == None/nil/undefined (Based on the language)
+
+// sum_pairs([10, 5, 2, 3, 7, 5],         10)
+// #              ^-----------^   5 + 5 = 10, indices: 1, 5
+// #                    ^--^      3 + 7 = 10, indices: 3, 4 *
+// #  * entire pair is earlier, and therefore is the correct answer
+// == [3, 7]
+
+export function sumPairs(ints: number[], s: number): [number, number] | void {
+  if (ints.length < 2) return undefined;
+
+  const indexSet = new Set();
+
+  for (let i = 0; i < ints.length; i++) {
+    const curr = ints[i];
+    const target = s - ints[i];
+
+    if (indexSet.has(target)) {
+      return [target, curr];
+    }
+
+    indexSet.add(curr);
+  }
+
+  return undefined;
+}
